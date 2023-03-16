df9eb02ec68d7efeaa35ea1d1be4eccd29b85409
MingHui <103197921+MingHui1997@users.noreply.github.com>
2023-03-16 22:47:58 +0800

    Add files via upload

---
+++ BMPRGBA/BMPRGBA.h
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+typedef struct _BMPRGBA_BmpInf
+{
+    unsigned int BmpWidth;
+    unsigned int BmpHeight;
+    int BmpBit;
+}BMPRGBA_BmpInf;
+
+void BMPRGBA_BmpGetInf(BMPRGBA_BmpInf *bmpinf,FILE *bmp_file);
+void BMPRGBA_BmpToRGBA(char *dest,BMPRGBA_BmpInf *bmpinf,FILE *bmp_file);
